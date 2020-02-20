9e7beda9f658d4d2d36c8169445dc72c2fb0fbd3
mihaiblandu <mihaiblandu@icloud.com>
2020-02-20 21:48:39 +0200

    JWT

---
+++ src/main/java/io/javabrains/jwt/model/AuthenticationResponse.java
@@ -0,0 +1,12 @@
+package io.javabrains.jwt.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@AllArgsConstructor
+public class AuthenticationResponse {
+    private  final String jwr;
+}
---
+++ src/main/java/io/javabrains/jwt/model/AutheticationRequest.java
@@ -0,0 +1,4 @@
+package io.javabrains.jwt.model;
+
+public class AutheticationRequest {
+}
